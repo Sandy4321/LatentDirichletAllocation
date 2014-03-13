bfb25b22025953ebbc698f68788b4ea954e3c821
Varad Pathak <pathak.varad@gmail.com>
2014-03-13 15:19:01 -0700

    Mini-Batches Created

--- TopicModeling/LDA/LDA.cpp
+++ TopicModeling/LDA/LDA.cpp
@@ -29,6 +29,15 @@ LDA::LDA(string file, int iter, int topics) {
 void LDA::parseDataFile() {
 	ifstream infile(fileName.c_str());
 
+	int nProcessors = omp_get_max_threads();
+	omp_set_num_threads(nProcessors);
+	int numOfMiniBatches = nProcessors;
+	std::vector<Document> *miniBatches[numOfMiniBatches];
+	int batchSize = (int) numOfDoc / (numOfMiniBatches);
+	for (int c = 0; c < numOfMiniBatches; ++c) {
+		miniBatches[c] = new vector<Document>();
+	}
+
 	infile >> numOfDoc;
 	infile >> numOfTerms;
 	infile >> numOfWordsInCorpus;
@@ -36,27 +45,43 @@ void LDA::parseDataFile() {
 	cout << "--------------------" << endl;
 	int docId, wordId, freq;
 	int eof = 0;
-	std::vector<Document> docVector;
 	if (!(infile >> docId >> wordId >> freq)) {
 		eof = 1;
 	}
-	while (!eof) {
-		intMap termMap;
-		int oldDocId = docId;
-		while (docId == oldDocId) {
-//			cout << docId << " " << wordId << " " << freq << endl;
-			termMap[wordId] = freq;
-			if (!(infile >> docId >> wordId >> freq)) {
-				eof = 1;
-				break;
-			}
+	int a = 0;
+#pragma omp parallel for shared(a) private(infile)
+	for (a = 0; a < numOfMiniBatches; ++a) {
+		int miniBatchStartDoc = batchSize * a;
+		while (docId != miniBatchStartDoc) {
+			infile >> docId >> wordId >> freq;
 		}
-		Document *newDoc = new Document(oldDocId, termMap);
-		docVector.push_back(*newDoc);
+		std::vector<Document> *docVector = miniBatches[a];
+		while (!eof) {
+			intMap termMap;
+			int oldDocId = docId;
+			while (docId == oldDocId) {
+				//			cout << docId << " " << wordId << " " << freq << endl;
+				termMap[wordId] = freq;
+				if (!(infile >> docId >> wordId >> freq)) {
+					eof = 1;
+					break;
+				}
+			}
+			Document *newDoc = new Document(oldDocId, termMap);
+			docVector->push_back(*newDoc);
+		}
+
 	}
-	SCVB0 *scvb0 = new SCVB0(iterations, numOfTopics, numOfTerms, numOfDoc,
-			numOfWordsInCorpus);
-	scvb0->run(docVector);
+	int b = 0;
+	for (b = 0; b < numOfMiniBatches; ++b) {
+		std::vector<Document> *docVector = miniBatches[a];
+		for (int i = 0; i < (int) docVector->size(); i++) {
+			//Document *D = docVector[i];
+		}
+//		SCVB0 *scvb0 = new SCVB0(iterations, numOfTopics, numOfTerms, numOfDoc,
+//				numOfWordsInCorpus);
+//		scvb0->run(*miniBatches[b]);
+	}
 	//return tEnd;
 }
 LDA *parseCommandLine(int argv, char *argc[]) {
@@ -73,10 +98,10 @@ LDA *parseCommandLine(int argv, char *argc[]) {
 
 int main(int argv, char *argc[]) {
 
-    double tStart = omp_get_wtime();
+	double tStart = omp_get_wtime();
 	LDA *lda = parseCommandLine(argv, argc);
 	lda->parseDataFile();
 	double tEnd = omp_get_wtime();
-	printf("Time taken: %.2fs\n", (double)(tEnd - tStart));
+	printf("Time taken: %.2fs\n", (double) (tEnd - tStart));
 	return 0;
 }
